4fd82fa66ce3ae6d76e8cadeb229bf4c9234cecf
Ubuntu <ubuntu@ip-172-31-83-203.ec2.internal>
2018-05-30 00:46:51 +0000

    Chef-Jenkins-Cookbook added

---
+++ jenkins-repo/cookbooks/example/recipes/jenkins.rb
@@ -0,0 +1,11 @@
+package 'apt-transport-https'
+
+apt_repository 'jenkins' do
+    uri          'https://pkg.jenkins.io/debian-stable'
+    distribution 'binary/'
+    key          'https://pkg.jenkins.io/debian-stable/jenkins.io.key'
+end
+
+package 'jenkins' do
+  
+end
